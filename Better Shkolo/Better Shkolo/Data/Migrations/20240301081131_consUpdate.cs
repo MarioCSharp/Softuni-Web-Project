1abe6373eaf1d97c96c06eb76e3b6a5555f3f0cc
MarioCSharp <mario.18@abv.bg>
2024-03-01 12:50:43 +0200

    Updates

---
+++ Better Shkolo/Better Shkolo/Data/Migrations/20240301081131_consUpdate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Better_Shkolo.Data.Migrations
+{
+    public partial class consUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Consultations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Consultations");
+        }
+    }
+}
---
+++ Better Shkolo/Better Shkolo/Models/Consultation/ConsultationMineModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Better_Shkolo.Models.Consultation
+{
+    public class ConsultationMineModel
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } = null!;
+        public string GradeName { get; set; } = null!;
+        public int SubjectId { get; set; }
+        public string UserId { get; set; } = null!;
+        public int GradeId { get; set; }
+        public int Term { get; set; }
+    }
+}
